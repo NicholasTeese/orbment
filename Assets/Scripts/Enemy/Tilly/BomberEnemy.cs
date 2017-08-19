e791e4c5834df8fbbfcae8a553088cbe76d2369a
STRONGISHLLAMA\StrongishLlama <taliesinwrmillhouse@gmail.com>
2017-08-19 22:21:40 +1000

    Started Bomber enemy.

---
+++ Assets/Scripts/Enemy/Tilly/BomberEnemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BomberEnemy : MonoBehaviour
+{
+    public enum Behaviour
+    {
+        WANDERING,
+        RETREATING,
+        HIDING
+    }
+
+    private float m_fMoveSpeed = 2.0f;
+
+    private Behaviour m_eBehaviour = Behaviour.WANDERING;
+
+    private NavMeshAgent m_navMeshAgent;
+
+    private void Awake()
+    {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+        m_navMeshAgent.speed = m_fMoveSpeed;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private Vector3 GetWanderPosition(Vector3 a_v3CurrentPosition)
+    {
+        float fXOffset = Random.Range(-5, 5);
+        float fZOffset = Random.Range(-5, 5);
+
+        return new Vector3(a_v3CurrentPosition.x + fXOffset, a_v3CurrentPosition.y, a_v3CurrentPosition.z + fZOffset);
+    }
