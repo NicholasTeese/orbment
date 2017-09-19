2dea13c339c1c04fd596b033b52fee9f7831aed0
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2017-09-19 19:24:20 +1000

    So weird.

---
+++ Assets/Scripts/KillBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    public List<Enemy> m_enemyList = new List<Enemy>();
+
+    public GameObject Ramp_1; public GameObject Ramp_2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(m_enemyList != null)
+            {
+                foreach(Enemy enemy in m_enemyList)
+                {
+                    enemy.m_currHealth = 0;
+                }
+            }
+            if(Ramp_1 != null)
+                Ramp_1.GetComponent<LiftManager>().Move = true;
+
+            if(Ramp_2 != null)
+                Ramp_2.GetComponent<LiftManager>().Move = true;
+        }
+    }
+}
