5d0c338c081d36d2a8b9cab0a13482c741704227
Nick/Home <Mr.Teese@hotmail.com>
2017-09-19 12:21:35 +1000

    Further work in Test Scene

--- Assets/Scripts/Managers/LiftManager.cs
+++ Assets/Scripts/Managers/LiftManager.cs
@@ -7,39 +7,40 @@ public class LiftManager : MonoBehaviour
     // Speed at which the platforms fall
     public float moveSpeed;
     // Bool to trigger the rise/fall of object
-    public bool Sink;
+    public bool Move;
 
     private float initPos;
-    private float lowerPos;
     public float TargetPos;
 
     // Use Awake for initializing
     void Awake()
     {
         initPos = transform.position.y;
-        lowerPos = transform.position.y - 10.0f;
     }
 
     // Update scene
     void Update()
     {
-        if (Sink)
+        if (Move)
         {
-            if (transform.position.y > TargetPos)
-                transform.position -= Vector3.up * moveSpeed * Time.deltaTime;
-            // Lower the object
+            if (this.name == "Wall")
+            {
+                //if (transform.position.y > TargetPos)
+                //    transform.position -= Vector3.up * moveSpeed * Time.deltaTime;
+                if (transform.position.y < TargetPos)
+                    transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+            }
+
+            if (this.name == "OrbRamp")
+                if (transform.position.y < TargetPos)
+                    transform.position += Vector3.up * moveSpeed * Time.deltaTime;
         }
         else
         {
-            if (transform.position.y < initPos)
-                transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-            // Raise the object
+//            if (transform.position.y < initPos)
+//                transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+//            // Raise the object
         }
-    }
-
-    void OnTriggerEnter()
-    {
-        Sink = false;
-        
     }
 }
