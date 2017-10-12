0cb2af72df5e3b3b1dce47498c28334b29af2527
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2017-10-12 15:01:36 +1100

    Implemented kill streak image.

--- Assets/Scripts/KillStreakManager/KillStreakManager.cs
+++ Assets/Scripts/KillStreakManager/KillStreakManager.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class KillStreakManager : MonoBehaviour
 {
-    public Text m_display;
-    public Text m_descriptionDisplay;
+    private Image m_currentKillStreakSprite = null;
+
+    public List<Sprite> m_killStreakSprite = new List<Sprite>();
 
     public string[] m_descriptions;
     public Color[] m_colours;
@@ -30,6 +31,91 @@ public class KillStreakManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        //m_killStreakSprite = InitialiseKillStreakSprites();
+    }
+
+    private void Start()
+    {
+        m_currentKillStreakSprite = PlayerHUDManager.m_playerHUDManager.transform.Find("Kill_Streak_Image").GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        DisplayKillStreak(m_killStreak);
+
+        if(!CheckKill())
+        {
+            ResetKillStreak();
+        }
+
+        Debug.Log(m_killStreak);
+    }
+
+    private List<Sprite> InitialiseKillStreakSprites()
+    {
+        List<Sprite> killStreakImages = new List<Sprite>();
+        killStreakImages.Add(Resources.Load("Textures/UI/KillStreak/2") as Sprite);
+        killStreakImages.Add(Resources.Load("Textures/UI/KillStreak/3") as Sprite);
+        killStreakImages.Add(Resources.Load("Textures/UI/KillStreak/4") as Sprite);
+        killStreakImages.Add(Resources.Load("Textures/UI/KillStreak/5") as Sprite);
+        killStreakImages.Add(Resources.Load("Textures/UI/KillStreak/6") as Sprite);
+        killStreakImages.Add(Resources.Load("Textures/UI/KillStreak/10") as Sprite);
+        return killStreakImages;
+    }
+
+    private void DisplayKillStreak(int a_iKillStreak)
+    {
+        switch (a_iKillStreak)
+        {
+            case 0:
+                {
+                    m_currentKillStreakSprite.gameObject.SetActive(false);
+                    break;
+                }
+
+            case 2:
+                {
+                    m_currentKillStreakSprite.gameObject.SetActive(true);
+                    m_currentKillStreakSprite.sprite = m_killStreakSprite[0];
+                    break;
+                }
+
+            case 3:
+                {
+                    m_currentKillStreakSprite.gameObject.SetActive(true);
+                    m_currentKillStreakSprite.sprite = m_killStreakSprite[1];
+                    break;
+                }
+
+            case 4:
+                {
+                    m_currentKillStreakSprite.gameObject.SetActive(true);
+                    m_currentKillStreakSprite.sprite = m_killStreakSprite[2];
+                    break;
+                }
+
+            case 5:
+                {
+                    m_currentKillStreakSprite.gameObject.SetActive(true);
+                    m_currentKillStreakSprite.sprite = m_killStreakSprite[3];
+                    break;
+                }
+
+            case 6:
+                {
+                    m_currentKillStreakSprite.gameObject.SetActive(true);
+                    m_currentKillStreakSprite.sprite = m_killStreakSprite[4];
+                    break;
+                }
+
+            case 10:
+                {
+                    m_currentKillStreakSprite.gameObject.SetActive(true);
+                    m_currentKillStreakSprite.sprite = m_killStreakSprite[5];
+                    break;
+                }
+        }
     }
 
     public void ResetKillStreak()
@@ -40,7 +126,7 @@ public class KillStreakManager : MonoBehaviour
 
     public void AddKill()
     {
-        if(CheckKill())
+        if (CheckKill())
         {
             m_killStreak++;
             m_timeOfLastKill = Time.time;
@@ -48,7 +134,7 @@ public class KillStreakManager : MonoBehaviour
             if (m_killStreak >= 5)
             {
                 Player.m_Player.m_currHealth += (Player.m_Player.m_maxHealth * 0.10f);
-//                Debug.Log(Player.m_Player.m_currHealth);
+                //                Debug.Log(Player.m_Player.m_currHealth);
             }
 
             if (m_killStreak >= 10)
@@ -57,36 +143,10 @@ public class KillStreakManager : MonoBehaviour
                 Player.m_Player.GodModeTimer = 5.0f;
             }
         }
-
-
-
     }
 
     public bool CheckKill()
     {
         return (m_timeOfLastKill == 0.0f || Time.time - m_timeOfLastKill <= m_timeAllowedBetweenKills);
-    }
-
-    private void Update()
-    {
-        if(m_display != null)
-        {
-            m_display.text = "x" + m_killStreak.ToString();
-        }
-
-        if(m_descriptionDisplay != null && m_killStreak < m_descriptions.Length)
-        {
-            m_descriptionDisplay.text = m_descriptions[m_killStreak];
-            if(m_killStreak < m_colours.Length)
-            {
-                m_display.color = m_colours[m_killStreak];
-                m_descriptionDisplay.color = m_colours[m_killStreak];
-            }
-        }
-
-        if(!CheckKill())
-        {
-            ResetKillStreak();
-        }
     }
 }
