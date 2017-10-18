2131e4991f06f3a532bbbdceb3b44d47cc88eb30
Nicholas <Mr.Teese@hotmail.com>
2017-10-18 14:40:23 +1100

    Beta prep
    Wall closes behind player after passing second door

--- Assets/Scripts/Tutorial/LiftManager.cs
+++ Assets/Scripts/Tutorial/LiftManager.cs
@@ -8,6 +8,8 @@ public class LiftManager : MonoBehaviour
     public float moveSpeed;
     // Bool to trigger the rise/fall of object
     public bool Move;
+    public GameObject wall;
+    public GameObject door;
 
     private float initPos;
     public float TargetPos;
@@ -15,7 +17,7 @@ public class LiftManager : MonoBehaviour
     // Use Awake for initializing
     void Awake()
     {
-        initPos = transform.position.y;
+        initPos = wall.transform.position.y;
     }
 
     // Update scene
@@ -23,24 +25,52 @@ public class LiftManager : MonoBehaviour
     {
         if (Move)
         {
-            if (this.name == "Wall")
-            {
+            //if (this.name == "Wall")
+            //{
+            if (wall != null)
+            {            
                 //if (transform.position.y > TargetPos)
                 //    transform.position -= Vector3.up * moveSpeed * Time.deltaTime;
-                if (transform.position.y < TargetPos)
-                    transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+                //if (transform.position.y < TargetPos)
+                //{
+                //    transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+                //}
+                if (wall.transform.position.y < TargetPos)
+                {
+                    wall.transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+                }
+                if (door != null)
+                {
+                    door.SetActive(false);
+                }
 
+            }
+            else
+            {
+                Debug.Log("Wall not found.");
             }
 
             if (this.name == "OrbRamp")
+            {
                 if (transform.position.y < TargetPos)
+                {
                     transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+                }
+            }
         }
         else
         {
 //            if (transform.position.y < initPos)
 //                transform.position += Vector3.up * moveSpeed * Time.deltaTime;
 //            // Raise the object
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Move = true;
         }
     }
 }
