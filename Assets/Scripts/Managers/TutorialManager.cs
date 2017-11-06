2e10cb9a70f6a5f88287868dea7086cd80cc32fe
Nicholas <Mr.Teese@hotmail.com>
2017-11-06 15:45:32 +1100

    Script work
    Added tutorial manageer script, working on message display

---
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    //public GameObject m_eMessageHolder;
+    public List<GameObject> m_MessageList = new List<GameObject>();
+
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    private int PopulateMessageList(List<GameObject> m_aMessageList)
+    {
+        int iMessageCount = 0;
+
+        for (int iCount = 0; iCount < m_aMessageList.Count; ++iCount)
+        {
+            iMessageCount += m_aMessageList[iCount].transform.childCount;
+        }
+
+        return iMessageCount;
+    }
+}
