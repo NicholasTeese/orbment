ffdf8fd1f7872cfec37dc951c98263c781a9d572
Nicholas <Mr.Teese@hotmail.com>
2017-11-09 11:03:06 +1100

    Lambert Data and Tutorial edits
    Tutorial Manager script work

--- Assets/Scripts/Managers/TutorialManager.cs
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    public static TutorialManager m_TutorialManager;
     //public GameObject m_eMessageHolder;
     public List<GameObject> m_MessageList = new List<GameObject>();
 
-	void Start()
+    public bool WindowOpen;
+
+    void Awake()
     {
-		
+        if (m_TutorialManager == null)
+        {
+            m_TutorialManager = this;
+        }
+        else if (m_TutorialManager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        InitList();
+        WindowOpen = false;
 	}
 	
 	void Update()
     {
-		
-	}
+        //foreach (GameObject a_message in m_MessageList)
+        //{
+        //    if (a_message.activeInHierarchy)
+        //    {
+        //        Time.timeScale = 0;
+        //    }
+        //}
+        if (WindowOpen)
+        {
+            Time.timeScale = 0;
+        }
+    }
 
     private int PopulateMessageList(List<GameObject> m_aMessageList)
     {
@@ -27,5 +53,39 @@ public class TutorialManager : MonoBehaviour
         }
 
         return iMessageCount;
+    }
+
+    private void InitList()
+    {
+        foreach (GameObject a_message in m_MessageList)
+        {
+            a_message.SetActive(false);
+        }
+    }
+
+    public void CloseWindow()
+    {
+        foreach (GameObject a_message in m_MessageList)
+        {
+            if (a_message.activeInHierarchy)
+            {
+                WindowOpen = false;
+                Time.timeScale = 1;
+                a_message.SetActive(false);
+            }
+        }
+    }
+
+    public void NextWindow()
+    {
+        foreach (GameObject a_message in m_MessageList)
+        {
+            if (a_message.activeInHierarchy)
+            {
+                WindowOpen = false;
+                Time.timeScale = 1;
+                a_message.SetActive(false);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public GameObject m_MessageHolder;
+
+	void Awake()
+    {
+
+    }
+	
+	void Update()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_MessageHolder != null && TutorialManager.m_TutorialManager != null)
+        {
+            m_MessageHolder.SetActive(true);
+            TutorialManager.m_TutorialManager.WindowOpen = true;
+        }
+        else
+        {
+            Debug.Log("Message holder = " + m_MessageHolder);
+            Debug.Log("Tutorial Manager = " + TutorialManager.m_TutorialManager);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.gameObject.SetActive(false);
+    }
+}
