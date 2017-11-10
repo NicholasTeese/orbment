3e1b3a97c955a7b083a56ab0074d5f665eb65e59
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2017-11-10 13:40:16 +1100

    UI Updates
    - Disabled tutorial UI when perk tree, pause or death menu are open
    - Fixed the perk tree particle effects being partially visable when you first open the perk tree

--- Assets/Scripts/Tutorial/TutorialTrigger.cs
+++ Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class TutorialTrigger : MonoBehaviour
 {
     private float m_fPopSpeed = 800.0f;
-    private float m_fPopDiatanceBuffer = 2.0f;
     private float m_fPoppedInHeight = 200.0f;
     private float m_fPoppedOutHeight = -150.0f;
 
     private bool m_bPopIn = false;
 
     public GameObject m_MessageHolder;
+    public GameObject m_tutorialCanvas;
 	
 	void Update()
     {
+        if (PerkTreeManager.m_perkTreeManager.gameObject.activeInHierarchy ||
+            PauseMenuManager.m_pauseMenuManager.gameObject.activeInHierarchy ||
+            DeathMenuManager.m_deathMenuManager.gameObject.activeInHierarchy)
+        {
+            m_tutorialCanvas.gameObject.SetActive(false);
+        }
+        else
+        {
+            m_tutorialCanvas.gameObject.SetActive(true);
+        }
+
 		if (m_bPopIn)
         {
             PopIn();
@@ -26,6 +37,14 @@ public class TutorialTrigger : MonoBehaviour
 	}
 
     private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_bPopIn = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
